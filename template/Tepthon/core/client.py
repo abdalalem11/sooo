253f7f977ceb28b95931b53b27eeb14feb543edb
abdalalem11 <abdalalem11@users.noreply.github.com>
2026-08-15 08:06:08 +0300

    fix factory database and Tepthon imports

--- template/Tepthon/core/client.py
+++ template/Tepthon/core/client.py
@@ -16,10 +16,10 @@ from telethon.errors import (
 from telethon.tl.types import User
 
 from Tepthon.config import Var
-from database import jmdB
+from Tepthon.database import jmdB
 from Tepthon.core.helper import fast_download
 from Tepthon.core.logger import LOGS, TelethonLogger
-from resources import blacklisted_users
+from Tepthon.resources import blacklisted_users
 
 
 class TepthonClient(TelegramClient):
